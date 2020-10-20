0791cb509f4e3d925a33877219e9df779fbf4f86
kien <trungkienbkhn123@gmail.com>
2020-10-20 16:47:10 +0700

    test api

--- hotel/urls.py
+++ hotel/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from django.conf.urls import url 
 from . import views
 
 urlpatterns = [
-    
+    url(r'^hotel/province$', views.province_list),
+    path('hotel/<int:pk>/', views.hotel_detail),
 ]
--- hotel/views.py
+++ hotel/views.py
@@ -3,37 +3,25 @@ from django.http import JsonResponse
 from django.core import serializers
 from django.http import HttpResponse
 import json
-from sqlalchemy import create_engine
-from clickhouse_sqlalchemy import make_session
-
-uri = 'clickhouse://CFF:Q3bohJmeuQcItP9vmhVE@phoenix-db.data.tripi.vn:443/PhoeniX?ssl=true&charset=utf8&protocol=https'
-engine = create_engine(uri)
-session = make_session(engine)
+from hotel.models import Province, Root, Url
 
 def hotel_list(request):
-    result = session.execute('''select *
-                                from roothotel_info
-                                limit 5''').fetchall()
+    a = Province.objects.all()
+    b = serializers.serialize('json', a)
+    return HttpResponse(b, content_type='application/json')
 
-    b = []
-    for i in range(len(result)):
-        a = {'id': result[i][0],'fields': {'name': result[i][1],'address': result[i][2]}}
-        b.append(a)
+def hotel_detail(request, pk):
+    if request.method == 'GET':
+        a = Root.objects.filter(province_id = pk)[:5]
+        b = serializers.serialize('json', a)
+        return HttpResponse(b, content_type='application/json')
 
-    c = json.dumps(b)
-
-    return HttpResponse(c, content_type="application/json")
-
-def hotel_detail(request, id):
-    result = session.execute('''select *
-                                from roothotel_info
-                                where id = :var''', {'var':  id}).fetchall()
-    b = []
-    for i in range(len(result)):
-        a = {'id': result[i][0],'fields': {'name': result[i][1],'address': result[i][2]}}
-        b.append(a)
-
-    c = json.dumps(b)
-
-    return HttpResponse(c, content_type="application/json")
-
+def province_list(request):
+    if request.method == 'GET':
+        province = Province.objects.all()    
+        name = request.GET.get('name', None)
+        if name is not None:
+            province = province.filter(name=name)
+        
+        b = serializers.serialize('json', province)
+        return HttpResponse(b, content_type='application/json')
